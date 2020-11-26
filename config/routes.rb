33dbb338d53bd1c22c43dd0bc760617784e4b655
yusuke0610 <74752096+yusuke0610@users.noreply.github.com>
2020-11-26 19:11:28 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
+  
 
 
-  resources :users, only: [:edit,:update]
+  resources :users, only: [:edit,:update,]
+  resources :rooms, only: [:new,:create,]
 end
